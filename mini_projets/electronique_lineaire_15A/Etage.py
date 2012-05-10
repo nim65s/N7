cba605be1454ddaaec51344a676b828a947414df
Guilhem Saurel <guilhem.saurel@gmail.com>
2012-05-10 20:25:20 +0200

    divisier pour mieux régner \o/

--- mini_projets/electronique_lineaire_15A/Etage.py
+++ mini_projets/electronique_lineaire_15A/Etage.py
@@ -2,6 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import shelve, os.path
+from mplib import *
 from logging import warning, error
 
 old_shelve = False
@@ -13,40 +14,6 @@ except:
     error('Ratage de l’ouverture du shelve')
     old = {}
 
-
-def par(a,b): return (a*b)/(a+b)
-
-kSI = ' kMGT'
-mSI = 'munp'
-def si(a):
-    if isinstance(a,str):
-        return a
-    if isinstance(a,minimaxi):
-        return str(a)
-    if a == 0 or abs(a) > 0.9 and abs(a) < 1.1:
-        return '%.2f' % a
-    if abs(a) > 1:
-        for i in range(len(kSI)):
-            if abs(a) < 1000:
-                break
-            a /= 1000
-        if i==0:
-            return '%.2f' % a
-        else:
-            return '%.2f%s' % (a, kSI[i])
-    else:
-        for i in range(len(mSI)):
-            a *= 1000
-            if abs(a) > 0.1:
-                break
-        return '%.2f%s' % (a, mSI[i])
-     
-def testsi():
-    for i in range(-12,+12):
-        print 0.9*10**i, si(0.9*10**i)
-        print 10**i, si(10**i)
-        print 1.1*10**i, si(1.1*10**i)
- 
 class AmplifierProperty(object):
     """ Classe remplançant la fonction buildin «property»,
     histoire d’éviter la duplication de code.
@@ -60,132 +27,6 @@ class AmplifierProperty(object):
     def __set__(self, obj, val):
         setattr(obj, self.name, float(val))
         obj.update()
-
-class minimaxi:
-    def __init__(self,mini,maxi):
-        self.mini = float(mini)
-        self.maxi = float(maxi)
-
-    def __eq__(self,a):
-        if isinstance(a,minimaxi):
-            return a.mini == self.mini and a.maxi == self.maxi
-        return a >= self.mini and a <= self.maxi
-
-    def __req__(self,a):
-        return self == a
-    
-    def __ne__(self,a):
-        return not self == a
-
-    def __rne__(self,a):
-        return self != a
-
-    def __gt__(self,a):
-        if isinstance(a,minimaxi):
-            return self.mini > a.mini and self.maxi > a.maxi
-        return self.mini > a
-
-    def __rgt__(self,a):
-        return self <= a
-
-    def __lt__(self,a):
-        if isinstance(a,minimaxi):
-            return self.mini < a.mini and self.maxi < a.maxi
-        return self.maxi < a
-
-    def __rlt__(self,a):
-        return self >= a
-
-    def __ge__(self,a):
-        return self == a or self > a
-
-    def __rge__(self,a):
-        return self < a
-
-    def __le__(self,a):
-        return self == a or self < a
-
-    def __rle__(self,a):
-        return self > a
-
-    def __neg__(self):
-        return minimaxi(-self.maxi, -self.mini)
-
-    def __add__(self,a):
-        if isinstance(a,minimaxi):
-            return minimaxi(self.mini+a.mini,self.maxi+a.maxi)
-        return minimaxi(self.mini+float(a),self.maxi+float(a))
-
-    def __radd__(self,a):
-        return self.__add__(float(a))
-
-    def __iadd__(self,a):
-        return self + a
-
-    def __sub__(self,a):
-        if isinstance(a,minimaxi):
-            return minimaxi(self.mini+a.mini,self.maxi+a.maxi)
-        return minimaxi(self.mini-float(a),self.maxi-float(a))
-
-    def __rsub__(self,a):
-        return self.__sub__(float(a))
-
-    def __isub__(self,a):
-        return self - a
-
-    def __mul__(self,a):
-        p = ''
-        if isinstance(a,minimaxi):
-            p = (
-                    self.mini*a.mini,
-                    self.mini*a.maxi,
-                    self.maxi*a.mini,
-                    self.maxi*a.maxi)
-        else:
-            p = ( self.mini*float(a), self.maxi*float(a))
-        return minimaxi(min(p),max(p))
-
-    def __rmul__(self,a):
-        return self.__mul__(float(a))
-
-    def __imul__(self,a):
-        return self * a
-
-    def __div__(self,a):
-        p = ''
-        if isinstance(a,minimaxi):
-            p = (
-                    self.mini/a.mini,
-                    self.mini/a.maxi,
-                    self.maxi/a.mini,
-                    self.maxi/a.maxi)
-        else:
-            p = (self.mini/float(a), self.maxi/float(a))
-        return minimaxi(min(p),max(p))
-
-    def __rdiv__(self,a):
-        p = ''
-        if isinstance(a,minimaxi):
-            p = (
-                    a.mini/self.mini,
-                    a.mini/self.maxi,
-                    a.maxi/self.mini,
-                    a.maxi/self.maxi)
-        else:
-            p = (float(a)/self.mini, float(a)/self.maxi)
-        return minimaxi(min(p),max(p))
-
-
-    def __idiv__(self,a):
-        return self/a
-
-    def __invert__(self):
-        return minimaxi(1/self.maxi,1/self.mini)
-
-    def __repr__(self):
-        if self.mini == self.maxi or (self.maxi-self.mini)/self.mini < 0.01:
-            return si(self.mini)
-        return '%s à %s'%(si(self.mini),si(self.maxi))
 
 class AbstractAmplifier(object):
     def __init__(self,Rb1,Rb2,Rc,Re,Cc=0,Ce=0,nom='',ve=0):
---
+++ mini_projets/electronique_lineaire_15A/mplib.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python2
+#-*- coding: utf-8 -*-
+
+def par(a,b): return (a*b)/(a+b)
+
+kSI = ' kMGT'
+mSI = 'munp'
+def si(a):
+    if isinstance(a,str):
+        return a
+    if isinstance(a,minimaxi):
+        return str(a)
+    if a == 0 or abs(a) > 0.9 and abs(a) < 1.1:
+        return '%.2f' % a
+    if abs(a) > 1:
+        for i in range(len(kSI)):
+            if abs(a) < 1000:
+                break
+            a /= 1000
+        if i==0:
+            return '%.2f' % a
+        else:
+            return '%.2f%s' % (a, kSI[i])
+    else:
+        for i in range(len(mSI)):
+            a *= 1000
+            if abs(a) > 0.1:
+                break
+        return '%.2f%s' % (a, mSI[i])
+     
+def testsi():
+    for i in range(-12,+12):
+        print 0.9*10**i, si(0.9*10**i)
+        print 10**i, si(10**i)
+        print 1.1*10**i, si(1.1*10**i)
+ 
+class minimaxi:
+    def __init__(self,mini,maxi):
+        self.mini = float(mini)
+        self.maxi = float(maxi)
+
+    def __eq__(self,a):
+        if isinstance(a,minimaxi):
+            return a.mini == self.mini and a.maxi == self.maxi
+        return a >= self.mini and a <= self.maxi
+
+    def __req__(self,a):
+        return self == a
+    
+    def __ne__(self,a):
+        return not self == a
+
+    def __rne__(self,a):
+        return self != a
+
+    def __gt__(self,a):
+        if isinstance(a,minimaxi):
+            return self.mini > a.mini and self.maxi > a.maxi
+        return self.mini > a
+
+    def __rgt__(self,a):
+        return self <= a
+
+    def __lt__(self,a):
+        if isinstance(a,minimaxi):
+            return self.mini < a.mini and self.maxi < a.maxi
+        return self.maxi < a
+
+    def __rlt__(self,a):
+        return self >= a
+
+    def __ge__(self,a):
+        return self == a or self > a
+
+    def __rge__(self,a):
+        return self < a
+
+    def __le__(self,a):
+        return self == a or self < a
+
+    def __rle__(self,a):
+        return self > a
+
+    def __neg__(self):
+        return minimaxi(-self.maxi, -self.mini)
+
+    def __add__(self,a):
+        if isinstance(a,minimaxi):
+            return minimaxi(self.mini+a.mini,self.maxi+a.maxi)
+        return minimaxi(self.mini+float(a),self.maxi+float(a))
+
+    def __radd__(self,a):
+        return self.__add__(float(a))
+
+    def __iadd__(self,a):
+        return self + a
+
+    def __sub__(self,a):
+        if isinstance(a,minimaxi):
+            return minimaxi(self.mini+a.mini,self.maxi+a.maxi)
+        return minimaxi(self.mini-float(a),self.maxi-float(a))
+
+    def __rsub__(self,a):
+        return self.__sub__(float(a))
+
+    def __isub__(self,a):
+        return self - a
+
+    def __mul__(self,a):
+        p = ''
+        if isinstance(a,minimaxi):
+            p = (
+                    self.mini*a.mini,
+                    self.mini*a.maxi,
+                    self.maxi*a.mini,
+                    self.maxi*a.maxi)
+        else:
+            p = ( self.mini*float(a), self.maxi*float(a))
+        return minimaxi(min(p),max(p))
+
+    def __rmul__(self,a):
+        return self.__mul__(float(a))
+
+    def __imul__(self,a):
+        return self * a
+
+    def __div__(self,a):
+        p = ''
+        if isinstance(a,minimaxi):
+            p = (
+                    self.mini/a.mini,
+                    self.mini/a.maxi,
+                    self.maxi/a.mini,
+                    self.maxi/a.maxi)
+        else:
+            p = (self.mini/float(a), self.maxi/float(a))
+        return minimaxi(min(p),max(p))
+
+    def __rdiv__(self,a):
+        p = ''
+        if isinstance(a,minimaxi):
+            p = (
+                    a.mini/self.mini,
+                    a.mini/self.maxi,
+                    a.maxi/self.mini,
+                    a.maxi/self.maxi)
+        else:
+            p = (float(a)/self.mini, float(a)/self.maxi)
+        return minimaxi(min(p),max(p))
+
+
+    def __idiv__(self,a):
+        return self/a
+
+    def __invert__(self):
+        return minimaxi(1/self.maxi,1/self.mini)
+
+    def __repr__(self):
+        if self.mini == self.maxi or (self.maxi-self.mini)/self.mini < 0.01:
+            return si(self.mini)
+        return '%s à %s'%(si(self.mini),si(self.maxi))
+
+
