03b438a1f7c7145838109b399091a1ff36e8fd48
Guilhem Saurel <guilhem.saurel@gmail.com>
2012-05-24 23:35:00 +0200

    premier étage avec Re1 != 0

--- mini_projets/electronique_lineaire_15A/premier.py
+++ mini_projets/electronique_lineaire_15A/premier.py
@@ -12,17 +12,15 @@ ORDRE = 0
 # Init
 nom='1CC'
 a = {}
-#iRb1=E(56000)
-#iRb2=E(68000)
-#iRe2=E(22000)
-iRb1=E(820000)
+iRb1=E(560000)
 iRb2=E(1000000)
-iRe2=E(39)
+iRe1=E(27)
+iRe2=E(15)
 Rc=E(100)
 Rg=50
 Zl=15000
 
-init = CollecteurCommun(Rb1=iRb1,Rb2=iRb2,Rc=Rc,Re1=0,Re2=iRe2,nom=nom,Rg=Rg,Zl=Zl,Ve=0.006)
+init = CollecteurCommun(Rb1=iRb1,Rb2=iRb2,Rc=Rc,Re1=iRe1,Re2=iRe2,nom=nom,Rg=Rg,Zl=Zl,Ve=0.006)
 print init
 
 Ze = minimaxi(25500,34500)
@@ -41,13 +39,13 @@ def erreurssi(cc):
 
 ZERO = set()
 
-def trouvermieuxboucle(Rb1,Rb2,Re2,Rc,nom,Rg,Zl,meilleur,meilleur_score):
+def trouvermieuxboucle(Rb1,Rb2,Re1,Re2,Rc,nom,Rg,Zl,meilleur,meilleur_score):
     """ ce qu’il se passe dans la boucle de trouvermieux """
-    cc = CollecteurCommun(Rb1=Rb1,Rb2=Rb2,Rc=Rc,Re1=0,Re2=Re2,nom=nom,Rg=Rg,Zl=Zl,Ve=0.006)
+    cc = CollecteurCommun(Rb1=Rb1,Rb2=Rb2,Rc=Rc,Re1=Re1,Re2=Re2,nom=nom,Rg=Rg,Zl=Zl,Ve=0.006)
     erreur = erreurs(cc)
     score = erreur[0] + erreur[1]
     if score == 0:
-        ZERO.add((Rb1.v,Rb2.v,Re2.v))
+        ZERO.add((Rb1.v,Rb2.v,Re1.v,Re2.v))
     elif score < meilleur_score:
         meilleur_score = score
         meilleur = cc
@@ -55,26 +53,31 @@ def trouvermieuxboucle(Rb1,Rb2,Re2,Rc,nom,Rg,Zl,meilleur,meilleur_score):
 
 def trouvermieux(icc):
     meilleur = icc
-    meilleur_score = 100000
+    meilleur_score = 1000000000000
 
     iRb1 = E(icc.Rb1)
     iRb2 = E(icc.Rb2)
-    iRe2 = E(icc.Re)
+    iRe1 = E(icc.Re1)
+    iRe2 = E(icc.Re2)
     sRb1 = [iRb1]
     sRb2 = [iRb2]
+    sRe1 = [iRe1]
     sRe2 = [iRe2]
     for i in range(1,5):
         sRb1 += [iRb1 + i]
         sRb1 += [iRb1 - i]
         sRb2 += [iRb2 + i]
         sRb2 += [iRb2 - i]
+        sRe1 += [iRe1 + i]
+        sRe1 += [iRe1 - i]
         sRe2 += [iRe2 + i]
         sRe2 += [iRe2 - i]
 
     for Rb1 in sRb1:
         for Rb2 in sRb2:
-            for Re2 in sRe2:
-                meilleur,meilleur_score = trouvermieuxboucle(Rb1,Rb2,Re2,icc.Rc,icc.nom,icc.Rg,icc.Zl,meilleur,meilleur_score)
+            for Re1 in sRe1:
+                for Re2 in sRe2:
+                    meilleur,meilleur_score = trouvermieuxboucle(Rb1,Rb2,Re1,Re2,icc.Rc,icc.nom,icc.Rg,icc.Zl,meilleur,meilleur_score)
     if not ZERO:
         print meilleur_score
     return meilleur, meilleur_score
@@ -90,7 +93,7 @@ def trouverlemeilleur(ZERO):
     meilleur_score = 0
     meilleur = ''
     for zRb1,zRb2,zRe1,zRe2 in ZERO:
-        cc = CollecteurCommun(Rb1=zRb1,Rb2=zRb2,Rc=Rc,Re1=0,Re2=zRe2,nom=nom,Rg=Rg,Zl=Zl,Ve=0.006)
+        cc = CollecteurCommun(Rb1=zRb1,Rb2=zRb2,Rc=Rc,Re1=zRe1,Re2=zRe2,nom=nom,Rg=Rg,Zl=Zl,Ve=0.006)
         scores = marge(cc)
         score = scores[0] + scores[1]
         if score > meilleur_score:
@@ -114,8 +117,8 @@ print len(ZERO)
 if len(ZERO):
     for i in range(ORDRE):
         ZEROcp = ZERO.copy()
-        for zRb1,zRb2,zRe2 in ZEROcp:
-            trouvermieux(CollecteurCommun(Rb1=zRb1,Rb2=zRb2,Rc=Rc,Re1=0,Re2=zRe2,nom=nom,Rg=Rg,Zl=Zl,Ve=0.006))
+        for zRb1,zRb2,zRe1,zRe2 in ZEROcp:
+            trouvermieux(CollecteurCommun(Rb1=zRb1,Rb2=zRb2,Rc=Rc,Re1=zRe1,Re2=zRe2,nom=nom,Rg=Rg,Zl=Zl,Ve=0.006))
         print len(ZERO)
     meilleur = trouverlemeilleur(ZERO)
     print meilleur
