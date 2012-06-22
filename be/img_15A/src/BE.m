613a8b4990ecd4d4ce26e829827749c4d45fe1fc
Guilhem Saurel <guilhem.saurel@gmail.com>
2012-06-22 14:22:02 +0200

    fin de la finalisation

--- be/img_15A/src/BE.m
+++ be/img_15A/src/BE.m
@@ -4,15 +4,15 @@ Zc = 50;
 vp = 2E8;
 d = 2;
 f = 13.56E6;
-M = M(Zc,vp,d,f)
-m_normalise = m_normalise(M,Zc)
-Z = Z(M)
-z_normalise = z_normalise(m_normalise)
-S = S(M, Zc)
+M0 = M(Zc,vp,d,f)
+m_normalise0 = m_normalise(M0,Zc)
+Z = Z(M0)
+z_normalise = z_normalise(m_normalise0)
+S = S(M0, Zc)
 
 %Reciproque : det(M) == 1
-if det(M) == 1
-    fprintf('La matrice est réciproque\n')
+if det(M0) == 1
+    fprintf('La matrice est rï¿½ciproque\n')
 end
 
 f=1E8:1E6:5E8;
@@ -23,44 +23,51 @@ fprintf('1c\n')
 f = 5E8;
 Yp = 10E-12*1i*2*pi*f + 1./(3.3E-9*1i*2*pi*f) + 1/4000;
 
-a1 = 1;
-b1 = 0;
-c1 = Yp*Zc;
-d1 = 1;
-
-m1 = [a1 b1;c1 d1]
+m1 = [1 0;Yp*Zc 1]
 
 f = 5E8:1E6:1E9;
 Yp = 10E-12*1i*2*pi*f + 1./(3.3E-9*1i*2*pi*f) + 1/4000;
 
-c1 = Yp/Zc;
+clear S11 S21
+for i = 1:501
+    Yp = 10E-12*1i*2*pi*f(i) + 1./(3.3E-9*1i*2*pi*f(i)) + 1/4000;
+    m1 = [1 0;Yp*Zc 1];
+    
+    M2 = M(Zc,vp,d,f(i));
+    m2 = m_normalise(M2,Zc);
 
-vp = 1E8;
-M2 = M(Zc,vp,d,f);
-m2 = m_normalise(M2,Zc);
+    m = m1*m2;
+    a = m(1,1);
+    b = m(1,2);
+    c = m(2,1);
+    d = m(2,2);
 
-m = m1*m2;
-[a b;c d] = m
-
-S11 = (a+b-c-d)./(a+b+c+d);
-S21 = 2./(a+b+c+d);
+    S11(i) = (a+b-c-d)./(a+b+c+d);
+    S21(i) = 2./(a+b+c+d);
+end
 
 plot(f,abs(S21),f,abs(S11))
 
 fprintf('1d\n')
+clear S11 S21
 f = 5E7:5E5:5E8;
-vp = 2E8;
-g = (2*pi*f)./vp;
+for i = 1:901
+    vp = 2E8;
+    g = (2*pi*f(i))./vp;
 
-m1 = [1 0; 1i*tan(g*2) 1]
+    m1 = [1 0; 1i*tan(g*2) 1];
 
-M2 = M(Zc,vp,2,f)
-m2 = m_normalise(M2,Zc);
+    M2 = M(Zc,vp,2,f);
+    m2 = m_normalise(M2,Zc);
 
-m = m1*m2;
-[a b;c d] = m
+    m = m1*m2;
+    a = m(1,1);
+    b = m(1,2);
+    c = m(2,1);
+    d = m(2,2);
 
-S11 = (a+b-c-d)./(a+b+c+d);
-S21 = (2)./(a+b+c+d);
+    S11(i) = (a+b-c-d)./(a+b+c+d);
+    S21(i) = (2)./(a+b+c+d);
+end
 
 plot(f,abs(S21),f,abs(S11))
