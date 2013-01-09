687877f0c3edd7d02fe60bb577432f0eddb89a49
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-01-09 16:21:15 +0100

    TPTNS: pas d’accents pour l’instant

--- TP/filtrage_2A/RIF.m
+++ TP/filtrage_2A/RIF.m
@@ -19,7 +19,7 @@ X = Fe * X / N;
 Y = 20 * log10(abs(fft(filter(coeff, 1, blanc))));
 
 plot(W .* Fe / (2 * pi), 20 * log10(abs(H)), X, Y(1:N/2), Xg, Ygb, Xg, Ygh);
-legend('Transmittance','bruit blanc filtré', 'gabarit min', 'gabarit max')
+legend('Transmittance','bruit blanc filtre', 'gabarit min', 'gabarit max')
 print -dpng RIF_1.png
 
 plot(W .* Fe / (2 * pi), unwrap(10 * phase(H)) / 10)  %ca n'a aucun sens !
@@ -34,5 +34,5 @@ fft_test = 20*log10(abs(fft(test)));
 f = 0 : N/2-1;
 test_f = 20 * log10(abs(fft(filter(coeff, 1, test))));
 plot(f(2:end), fft_test(2:N/2), f(2:end), test_f(2:N/2));
-legend('signal de test', 'signal de test filtré')
+legend('signal de test', 'signal de test filtre')
 print -dpng RIF_3.png
